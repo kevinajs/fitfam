abc8561eb26c5068fbfce993863f894ed3673f3c
Kevin Saunders <kevinajsaunders@gmail.com>
2020-06-26 18:43:49 -0400

    Added in logic making payments, displaying payments, displaying remaining balances.

--- carpaymentscalculator/app/forms.py
+++ carpaymentscalculator/app/forms.py
@@ -1,9 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import IntegerField, SubmitField
 from wtforms.validators import DataRequired
 
-class SelectionForm(FlaskForm):
-	username = StringField('Username', validators=[DataRequired()])
-	password = PasswordField('Password', validators=[DataRequired()])
-	remember_me = BooleanField('Remember Me')
-	submit = SubmitField('Sign In')
+class PaymentForm(FlaskForm):
+	payment_amount = IntegerField('Payment Amount', validators=[DataRequired()])
+	submit = SubmitField('Submit Payment')
+
--- carpaymentscalculator/app/routes.py
+++ carpaymentscalculator/app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template, flash, redirect, url_for
 from app import app, db
+from app.forms import PaymentForm
 from app.models import Payment
 
 @app.route('/')
@@ -12,37 +13,54 @@ def index():
 
 @app.route('/balance')
 def balance():
-	title = 'View Payments'
+	title = 'Check Balance'
+	TOTAL_AMOUNT = 8631.48
+	query = db.engine.execute("""
+	                                SELECT SUM(payment_amount)
+	                                FROM Payment
+	                                """
+	                                ).fetchone()
+	amount_paid = query[0]
+	to_be_paid = TOTAL_AMOUNT - query[0]
 
 	return render_template('balance.html',
-	                       title=title)
+	                       title=title,
+	                       total_amount='${:,.2f}'.format(TOTAL_AMOUNT),
+	                       amount_paid='${:,.2f}'.format(amount_paid),
+	                       to_be_paid='${:,.2f}'.format(to_be_paid))
 
-@app.route('/payments')
+@app.route('/payments', methods=['GET','POST'])
 def payment():
 	title = 'Make a Payment'
-
+	form = PaymentForm()
+	if form.validate_on_submit():
+		payment = Payment(payment_amount=form.payment_amount.data)
+		print(payment)
+		db.session.add(payment)
+		db.session.commit()
+		flash('Payment Successful!')
+		return redirect('payments')
 	return render_template('payments.html',
-	                       title=title)
+	                       title=title,
+	                       form=form)
 
 @app.route('/viewpayments', methods=['GET','POST'])
 def viewpayments():
 	title = 'View a Payment'
-	results = db.engine.execute('SELECT date_recorded, SUM(payment_amount) FROM Payment GROUP BY date_recorded').fetchall()
-	print(type(results))
-	print(results)
-	for row in results:
-		for x in row:
-			print(x)
+	query = db.engine.execute("""
+	                            SELECT	DATE(date_recorded) AS date_recorded
+	                            		,SUM(payment_amount) AS payment_amount
+	                            FROM Payment
+	                            GROUP BY DATE(date_recorded)
+	                            """)
 
-	for row in results:
-		for x in row:
-			print(x)
+	results = query.fetchall()
 
-
-	returned = [row[0] for row in results]
+	column_names = [col for col in query.keys()]
 
 	return render_template('viewpayments.html',
 	                       title=title,
-	                       id = returned
+	                       column_names=column_names,
+	                       results=results
 	                       )
 
