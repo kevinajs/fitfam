91679f44359c8b017fcf1c70e11a7f4d5ec376f0
Kevin Saunders <kevinajsaunders@gmail.com>
2020-08-08 11:08:13 -0400

    Created Registration front end page.

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,24 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField
 from wtforms.validators import DataRequired
+
+GENDER_CHOICES = ['', 'Male', 'Female', 'Prefer Not to Answer']
+
+class RegistrationForm(FlaskForm):
+	first_name = StringField('First Name', validators=[DataRequired()])
+	last_name = StringField('Last Name', validators=[DataRequired()])
+	gender = SelectField('Gender', choices=GENDER_CHOICES, validators=[DataRequired()])
+	username = StringField('Username', validators=[DataRequired()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	submit = SubmitField('Submit')
+
+	#birthdate
+	#email
 
 class LoginForm(FlaskForm):
 	username = StringField('Username', validators=[DataRequired()])
 	password = PasswordField('Password', validators=[DataRequired()])
 	remember_me = BooleanField('Remember Me')
 	submit = SubmitField('Sign In')
+
+
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,18 @@
 from flask import render_template
 from app import app
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('base.html', title='Display this shit')
 
+@app.route('/register')
+def register():
+	form = RegistrationForm()
+	return render_template('registration.html', form=form)
+
 @app.route('/login')
 def login():
 	form = LoginForm()
-	return render_template('login.html', form = form)
+	return render_template('login.html', form=form)
